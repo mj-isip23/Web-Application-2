258b7e67a181fdd8889384e316983b0d663bfb63
Michael John Isip <mj.isip123@gmail.com>
2017-03-13 04:53:48 +0800

    Add files via upload

---
+++ database/migrations/2017_02_17_193200_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('studNumber', 10);
+            $table->string('studName', 50);
+            $table->string('course', 50);
+            $table->integer('year', 5);
+            $table->string('address', 200);            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
